3f0335f0d08146e3c817fbf3ca32363ac9eb7678
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-08-09 14:14:11 -0400

    Initial Commit

---
+++ Gallo-SwiftUI/Gallo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gallo_SwiftUIApp.swift
+//  Gallo-SwiftUI
+//
+//  Created by Danny on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Gallo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
